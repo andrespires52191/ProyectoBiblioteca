688ba7bf7357a8a6f9577f94c4d394559704f9b0
148fa12 <andres.pires.52191@ikasle.egibide.org>
2026-01-26 20:40:57 +0100

    Vistas de gestion

---
+++ ProyectoBiblioteca/vista/GestionUsuarios.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace ProyectoBiblioteca.vista
+{
+    partial class GestionUsuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tlpPrincipal = new System.Windows.Forms.TableLayoutPanel();
+            this.tlpDatosTitulos = new System.Windows.Forms.TableLayoutPanel();
+            this.tlpDatosExterior = new System.Windows.Forms.TableLayoutPanel();
+            this.lTituloNombre = new System.Windows.Forms.Label();
+            this.lTituloApellido1 = new System.Windows.Forms.Label();
+            this.lTituloApellido2 = new System.Windows.Forms.Label();
+            this.lAcciones = new System.Windows.Forms.Label();
+            this.lCabecera = new System.Windows.Forms.Label();
+            this.tlpDatosInterior = new System.Windows.Forms.TableLayoutPanel();
+            this.tlpPrincipal.SuspendLayout();
+            this.tlpDatosTitulos.SuspendLayout();
+            this.tlpDatosExterior.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tlpPrincipal
+            // 
+            this.tlpPrincipal.AutoSize = true;
+            this.tlpPrincipal.ColumnCount = 3;
+            this.tlpPrincipal.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpPrincipal.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpPrincipal.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpPrincipal.Controls.Add(this.tlpDatosExterior, 1, 2);
+            this.tlpPrincipal.Controls.Add(this.lCabecera, 1, 1);
+            this.tlpPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.tlpPrincipal.Name = "tlpPrincipal";
+            this.tlpPrincipal.RowCount = 4;
+            this.tlpPrincipal.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpPrincipal.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tlpPrincipal.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpPrincipal.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpPrincipal.Size = new System.Drawing.Size(800, 450);
+            this.tlpPrincipal.TabIndex = 3;
+            // 
+            // tlpDatosTitulos
+            // 
+            this.tlpDatosTitulos.AutoSize = true;
+            this.tlpDatosTitulos.ColumnCount = 4;
+            this.tlpDatosTitulos.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tlpDatosTitulos.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tlpDatosTitulos.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tlpDatosTitulos.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tlpDatosTitulos.Controls.Add(this.lTituloNombre, 0, 0);
+            this.tlpDatosTitulos.Controls.Add(this.lTituloApellido1, 1, 0);
+            this.tlpDatosTitulos.Controls.Add(this.lTituloApellido2, 2, 0);
+            this.tlpDatosTitulos.Controls.Add(this.lAcciones, 3, 0);
+            this.tlpDatosTitulos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpDatosTitulos.Location = new System.Drawing.Point(3, 3);
+            this.tlpDatosTitulos.Name = "tlpDatosTitulos";
+            this.tlpDatosTitulos.RowCount = 1;
+            this.tlpDatosTitulos.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tlpDatosTitulos.Size = new System.Drawing.Size(748, 50);
+            this.tlpDatosTitulos.TabIndex = 1;
+            // 
+            // tlpDatosExterior
+            // 
+            this.tlpDatosExterior.AutoSize = true;
+            this.tlpDatosExterior.ColumnCount = 1;
+            this.tlpDatosExterior.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpDatosExterior.Controls.Add(this.tlpDatosTitulos, 0, 0);
+            this.tlpDatosExterior.Controls.Add(this.tlpDatosInterior, 0, 1);
+            this.tlpDatosExterior.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpDatosExterior.Location = new System.Drawing.Point(23, 36);
+            this.tlpDatosExterior.Name = "tlpDatosExterior";
+            this.tlpDatosExterior.RowCount = 2;
+            this.tlpDatosExterior.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tlpDatosExterior.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpDatosExterior.Size = new System.Drawing.Size(754, 391);
+            this.tlpDatosExterior.TabIndex = 2;
+            // 
+            // lTituloNombre
+            // 
+            this.lTituloNombre.AutoSize = true;
+            this.lTituloNombre.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lTituloNombre.Location = new System.Drawing.Point(3, 0);
+            this.lTituloNombre.Name = "lTituloNombre";
+            this.lTituloNombre.Size = new System.Drawing.Size(209, 50);
+            this.lTituloNombre.TabIndex = 0;
+            this.lTituloNombre.Text = "Nombre";
+            this.lTituloNombre.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lTituloApellido1
+            // 
+            this.lTituloApellido1.AutoSize = true;
+            this.lTituloApellido1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lTituloApellido1.Location = new System.Drawing.Point(218, 0);
+            this.lTituloApellido1.Name = "lTituloApellido1";
+            this.lTituloApellido1.Size = new System.Drawing.Size(210, 50);
+            this.lTituloApellido1.TabIndex = 1;
+            this.lTituloApellido1.Text = "1º Apellido";
+            this.lTituloApellido1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lTituloApellido2
+            // 
+            this.lTituloApellido2.AutoSize = true;
+            this.lTituloApellido2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lTituloApellido2.Location = new System.Drawing.Point(434, 0);
+            this.lTituloApellido2.Name = "lTituloApellido2";
+            this.lTituloApellido2.Size = new System.Drawing.Size(210, 50);
+            this.lTituloApellido2.TabIndex = 2;
+            this.lTituloApellido2.Text = "2º Apellido";
+            this.lTituloApellido2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lAcciones
+            // 
+            this.lAcciones.AutoSize = true;
+            this.lAcciones.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lAcciones.Location = new System.Drawing.Point(650, 0);
+            this.lAcciones.Name = "lAcciones";
+            this.lAcciones.Size = new System.Drawing.Size(95, 50);
+            this.lAcciones.TabIndex = 3;
+            this.lAcciones.Text = "Acciones";
+            this.lAcciones.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lCabecera
+            // 
+            this.lCabecera.AutoSize = true;
+            this.lCabecera.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lCabecera.Location = new System.Drawing.Point(23, 20);
+            this.lCabecera.Name = "lCabecera";
+            this.lCabecera.Size = new System.Drawing.Size(754, 13);
+            this.lCabecera.TabIndex = 3;
+            this.lCabecera.Text = "Gestión de Usuarios";
+            // 
+            // tlpDatosInterior
+            // 
+            this.tlpDatosInterior.ColumnCount = 1;
+            this.tlpDatosInterior.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpDatosInterior.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpDatosInterior.Location = new System.Drawing.Point(3, 59);
+            this.tlpDatosInterior.Name = "tlpDatosInterior";
+            this.tlpDatosInterior.RowCount = 1;
+            this.tlpDatosInterior.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpDatosInterior.Size = new System.Drawing.Size(748, 329);
+            this.tlpDatosInterior.TabIndex = 2;
+            // 
+            // GestionUsuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tlpPrincipal);
+            this.Name = "GestionUsuarios";
+            this.Text = "GestionUsuarios";
+            this.tlpPrincipal.ResumeLayout(false);
+            this.tlpPrincipal.PerformLayout();
+            this.tlpDatosTitulos.ResumeLayout(false);
+            this.tlpDatosTitulos.PerformLayout();
+            this.tlpDatosExterior.ResumeLayout(false);
+            this.tlpDatosExterior.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpPrincipal;
+        private System.Windows.Forms.TableLayoutPanel tlpDatosExterior;
+        private System.Windows.Forms.TableLayoutPanel tlpDatosTitulos;
+        private System.Windows.Forms.Label lTituloNombre;
+        private System.Windows.Forms.Label lTituloApellido1;
+        private System.Windows.Forms.Label lTituloApellido2;
+        private System.Windows.Forms.Label lAcciones;
+        private System.Windows.Forms.Label lCabecera;
+        private System.Windows.Forms.TableLayoutPanel tlpDatosInterior;
+    }
+}
